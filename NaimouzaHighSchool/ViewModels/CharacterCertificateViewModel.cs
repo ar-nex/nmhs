44e4ae0d7f5a352d16a2576169ef0ae6fad65e7c
ami <err7rr@hotmail.com>
2017-04-09 00:24:56 +0530

    Character certificate updated

--- NaimouzaHighSchool/ViewModels/CharacterCertificateViewModel.cs
+++ NaimouzaHighSchool/ViewModels/CharacterCertificateViewModel.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Globalization;
 using NaimouzaHighSchool.ViewModels.Commands;
 using NaimouzaHighSchool.ViewModels.Helpers;
+using NaimouzaHighSchool.Models.Utility;
+using NaimouzaHighSchool.Models.Database;
 
 using MigraModel = MigraDoc.DocumentObjectModel;
 using MigraDoc.Rendering;
@@ -22,11 +24,19 @@ namespace NaimouzaHighSchool.ViewModels
             : base()
         {
             this.GenerateCertificateCommand = new RelayCommand(GenerateCertificate, CanGenerateCertificate);
+            this.GetStudentDataCommand = new RelayCommand(this.GetBtnClicked, this.CanGetBtnClicked);
             this.CertifHelper = new CertificateHelper();
+            this.Session = DateTime.Today.Year.ToString();
+            this.SessionEnd = DateTime.Today.Year.ToString();
             this.InitializeStartUpVisibility();
             this.InitializeStartUpComboList();
             this.IncludeHeader = true;
-            
+            this.SelectedSectionIndex = -1;
+            this.SelectedClassIndex = -1;
+            this.SearchTypeIndex = -1;
+            this.GenderIndex = -1;
+            this.GetBtnContent = "?";
+            this.PreviewText = string.Empty;
         }
 
         #region fields
@@ -88,7 +98,20 @@ namespace NaimouzaHighSchool.ViewModels
             get { return _includeHeader; }
             set { _includeHeader = value; this.OnPropertyChanged("IncludeHeader"); }
         }
-        // testing.
+
+        private int _seachTypeIndex;
+        public int SearchTypeIndex
+        {
+            get { return this._seachTypeIndex; }
+            set 
+            {
+                this._seachTypeIndex = value;
+                this.SelectedSearchType = (value > -1) ? this.SearchType[value] : string.Empty;
+                this.Reset();
+                this.OnPropertyChanged("SearchTypeIndex");
+            }
+        }
+        private int _searchTypeFlag = 0;
         public string SelectedSearchType
         {
             get { return _selectedSearchType; }
@@ -101,16 +124,21 @@ namespace NaimouzaHighSchool.ViewModels
                         this.VisibilityUniqueIndividual = Visibility.Collapsed;
                         this.VisibilityClassList = Visibility.Collapsed;
                         this.VisibilityNameList = Visibility.Collapsed;
-                        this.VisibilityAddhoc = Visibility.Visible;                      
+                        this.VisibilityAddhoc = Visibility.Visible;
+                        this._searchTypeFlag = 1;
+                        this.GetBtnContent = "Preview";
                         break;
                     case "By Class, Section and Roll":
                         this.VisibilityClassList = Visibility.Collapsed;
                         this.VisibilityNameList = Visibility.Collapsed;
                         this.VisibilityAddhoc = Visibility.Collapsed;
                         this.VisibilityUniqueIndividual = Visibility.Visible;
+                        this._searchTypeFlag = 2;
+                        this.GetBtnContent = "Get";
                         break;
 
                     default:
+                        this._searchTypeFlag = 0;
                         break;
                 }
                 this.OnPropertyChanged("SelectedSearchType");
@@ -253,7 +281,19 @@ namespace NaimouzaHighSchool.ViewModels
             set
             {
                 _stdGender = value;
-                this.OnPropertyChanged("StdName");
+                this.OnPropertyChanged("StdGender");
+            }
+        }
+
+        private int _genderIndex;
+        public int GenderIndex
+        {
+            get { return this._genderIndex; }
+            set
+            {
+                this._genderIndex = value;
+                this.StdGender = (value > -1) ? this.Gender[value] : string.Empty;
+                this.OnPropertyChanged("GenderIndex");
             }
         }
         public string SelectedClass
@@ -278,20 +318,78 @@ namespace NaimouzaHighSchool.ViewModels
             get { return _session; }
             set { _session = value; this.OnPropertyChanged("Session"); }
         }
+
+        private string _getBtnContent;
+        public string GetBtnContent
+        {
+            get { return this._getBtnContent; }
+            set { this._getBtnContent = value; this.OnPropertyChanged("GetBtnContent"); }
+        }
         public string Dob
         {
             get { return _dob; }
             set { _dob = value; this.OnPropertyChanged("Dob"); }
         }
 
-        private string _textPreview;
-        public string TextPreview
+        private string _address;
+        public string Address
         {
-            get { return this._textPreview; }
-            set { this._textPreview = value; this.OnPropertyChanged("TextPreview"); }
+            get { return this._address; }
+            set
+            {
+                this._address = value;
+                if (!string.IsNullOrEmpty(value) && (this.SelectedClassIndex > -1) && (this.SelectedSectionIndex > -1))
+                {
+                    this.BuildPreview();
+                }
+                this.OnPropertyChanged("Address");
+            }
         }
 
+        private string _sessionEnd;
+        public string SessionEnd
+        {
+            get { return this._sessionEnd; }
+            set
+            {
+                this._sessionEnd = value;
+                this.OnPropertyChanged("SessionEnd");
+            }
+            
+        }
+
+        private int _selectedClassIndex;
+        public int SelectedClassIndex
+        {
+            get { return this._selectedClassIndex; }
+            set
+            {
+                this._selectedClassIndex = value;
+                this.SelectedClass = (value > -1) ? this.SchoolClass[value] : string.Empty;
+                this.OnPropertyChanged("SelectedClassIndex");
+            }
+        }
+
+        private int _selectedSectionIndex;
+        public int SelectedSectionIndex
+        {
+            get { return this._selectedSectionIndex; }
+            set 
+            {
+                this._selectedSectionIndex = value;
+                this.SelectedSection = (value > -1) ? this.SchoolSection[value] : string.Empty;
+                this.OnPropertyChanged("SelectedSectionIndex");
+            }
+        }
+
+        private string _previewText;
+        public string PreviewText
+        {
+            get { return this._previewText; }
+            set { this._previewText = value; this.OnPropertyChanged("PreviewText"); }
+        }
         public RelayCommand GenerateCertificateCommand { get; set; }
+        public RelayCommand GetStudentDataCommand { get; set; }
         #endregion
 
 
@@ -299,47 +397,79 @@ namespace NaimouzaHighSchool.ViewModels
         #region method
         public void GenerateCertificate()
         {
-            string[] cerficateData = new string[13];
-            cerficateData[0] = this.StdName;
-            cerficateData[1] = this.FatherName;
-            cerficateData[2] = this.Vill;
-            cerficateData[3] = this.PO;
-            cerficateData[4] = this.Ps;
-            cerficateData[5] = this.Dist;
-            cerficateData[6] = this.Pin;
-            cerficateData[7] = this.StdGender;
-            cerficateData[8] = this.SelectedClass;
-            cerficateData[9] = this.SelectedSection;
-            cerficateData[10] = this.Roll;
-            cerficateData[11] = this.Session;
-            cerficateData[12] = this.Dob;
-
-            try
+            switch (this._searchTypeFlag)
             {
-                 CertifHelper.CreateCharacterCertificatePDF(cerficateData);
+                case 1:
+                    string[] certificateData = new string[5];
+                    char[] seps = { '\n'};
+                    string[] data_temp = this.PreviewText.Split(seps);
+                    for (int i = 0; i < data_temp.Length; i++)
+                    {
+                        certificateData [i] = data_temp[i];
+                    }
+                    certificateData[4] = this.StdName;
+
+                    try
+                    {
+                        CertifHelper.CreateCharacterCertificatePDF(certificateData);
+                        this.Reset();
+                    }
+                    catch (Exception em)
+                    {
+                        System.Windows.MessageBox.Show(em.Message);
+                    }
+                    break;
+                
+                case 2 :
+                    string[] certificateData2 = new string[5];
+                    char[] seps2 = { '\n'};
+                    string[] data_temp2 = this.PreviewText.Split(seps2);
+                    for (int i = 0; i < data_temp2.Length; i++)
+                    {
+                        certificateData2 [i] = data_temp2[i];
+                    }
+                    certificateData2[4] = this.StdName;
+                    try
+                    {                       
+                        CertifHelper.CreateCharacterCertificatePDF(certificateData2);
+                        this.Reset();
+                    }
+                    catch (Exception em)
+                    {
+                        System.Windows.MessageBox.Show(em.Message);
+                    }
+
+                    break;
+                default:
+                    break;
             }
-            catch (Exception em)
-            {
-
-                System.Windows.MessageBox.Show(em.Message);
-            }
-
-
-            
+           
 
         }
+
         public bool CanGenerateCertificate()
         {
-            return true;
+            bool r = false;
+            switch (this._searchTypeFlag)
+            {
+                case 1:
+                    r = (string.IsNullOrEmpty(this.StdName) || string.IsNullOrEmpty(this.FatherName) || string.IsNullOrEmpty(this.Vill) || string.IsNullOrEmpty(this.PO) || string.IsNullOrEmpty(this.Ps) || string.IsNullOrEmpty(this.Dist) || string.IsNullOrEmpty(this.Pin) || string.IsNullOrEmpty(this.StdGender) || string.IsNullOrEmpty(this.Dob) || string.IsNullOrEmpty(this.Roll) || (this.SelectedClassIndex == -1) || (this.SelectedSectionIndex == -1) || (string.IsNullOrEmpty(this.Session)) || (string.IsNullOrEmpty(this.SessionEnd))) ? false : true;
+                    break;
+                case 2:
+                    r = (string.IsNullOrEmpty(this.Roll) || (this.SelectedClassIndex == -1) || (this.SelectedSectionIndex == -1) || (string.IsNullOrEmpty(this.Session)) || (string.IsNullOrEmpty(this.SessionEnd))) ? false : true;
+                    break;
+                default:
+                    r = false;
+                    break;
+            }
+            return r;
         }
         private void InitializeStartUpVisibility()
         {
-            this.VisibilityAddhoc = Visibility.Visible;
+            this.VisibilityAddhoc = Visibility.Collapsed;
             this.VisibilityUniqueIndividual = Visibility.Collapsed;
             this.VisibilityClassList = Visibility.Collapsed;
             this.VisibilityNameList = Visibility.Collapsed;
-
-           
         }
         private void InitializeStartUpComboList()
         {
@@ -401,9 +531,111 @@ namespace NaimouzaHighSchool.ViewModels
             SchoolSection.Add("D");
             SchoolSection.Add("E");
         }
-        #endregion
-      
 
-      
+        private void GetBtnClicked()
+        {
+            switch (this._searchTypeFlag)
+            {
+                case 1:
+                    this.BuildPreview();
+                    break;
+
+                case 2:
+                    string[] searchData = new string[] { this.SelectedClass, this.SelectedSection, this.Roll, this.Session, this.SessionEnd};
+                    CharacterCertificateDb db = new CharacterCertificateDb();
+                    Student s = db.GetData(searchData);
+                    if (string.IsNullOrEmpty(s.Name))
+                    {
+                        MessageBox.Show("No such student found.");
+                        return;
+                    }
+                    else
+                    {
+                        this.StdName = s.Name;
+                        this.FatherName = s.FatherName;
+                        this.Address = s.PresentAdrress;
+                        if (s.Sex == "M")
+                        {
+                            this.GenderIndex = 0;
+                        }
+                        else if (s.Sex == "F")
+                        {
+                            this.GenderIndex = 1;
+                        }
+                        else
+                        {
+                            this.GenderIndex = -1;
+                        }
+                        this.Dob = s.Dob.ToString("dd-MM-yyyy");
+                        this.BuildPreview();      
+                     }
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+
+        private bool CanGetBtnClicked()
+        {
+            bool r = false;
+            switch (this._searchTypeFlag)
+            {
+                case 1:
+                    r = (string.IsNullOrEmpty(this.StdName) || string.IsNullOrEmpty(this.FatherName) || string.IsNullOrEmpty(this.Vill) || string.IsNullOrEmpty(this.PO) || string.IsNullOrEmpty(this.Ps) || string.IsNullOrEmpty(this.Dist) || string.IsNullOrEmpty(this.Pin) || string.IsNullOrEmpty(this.StdGender) || string.IsNullOrEmpty(this.Dob) || string.IsNullOrEmpty(this.Roll) || (this.SelectedClassIndex == -1) || (this.SelectedSectionIndex == -1) || (string.IsNullOrEmpty(this.Session)) || (string.IsNullOrEmpty(this.SessionEnd))) ? false : true;
+                    break;
+                case 2:
+                    r = (string.IsNullOrEmpty(this.Roll) || (this.SelectedClassIndex == -1) || (this.SelectedSectionIndex == -1) || (string.IsNullOrEmpty(this.Session)) || (string.IsNullOrEmpty(this.SessionEnd))) ? false : true;
+                    break;
+                default:
+                    r = false;
+                    break;
+            }
+            return r;
+        }
+
+        private void BuildPreview()
+        {
+            string xchild = (this.StdGender == "Boy") ? "son" : "daughter";
+            string xhe = (this.StdGender == "Boy") ? "he" : "she";
+            string Xhe = (this.StdGender == "Boy") ? "He" : "She";
+            string Hir = (this.StdGender == "Boy") ? "His" : "Her";
+            string hir = (this.StdGender == "Boy") ? "his" : "her";
+
+            int currentYear = DateTime.Today.Year;
+            int sessStart = 0;
+            int sessEnd = 0;
+            try 
+	            {	        
+		             sessStart = Int32.Parse(this.Session);
+                     sessEnd = Int32.Parse(this.SessionEnd);
+	            }
+	            catch (Exception year)
+	            {
+		            MessageBox.Show("Year conversion : "+year.Message); 
+	            }
+            string wis = (sessStart == currentYear || sessEnd == currentYear) ? "is" : "was";
+            string preview1 = string.Empty;
+            if (this._searchTypeFlag == 1)
+            {
+                preview1 = "  This is to certify that " + this.StdName + ", " + xchild + " of " + this.FatherName + ", Vill. " + this.Vill + ", P.O. " + this.PO + ", P.S. " + this.Ps + ", Dist. " + this.Dist + ", PIN-" + this.Pin + ", is personally known to me.";
+            }
+            else if(this._searchTypeFlag == 2)
+            {
+                preview1 = "  This is to certify that " + this.StdName + ", " + xchild + " of " + this.FatherName + ", " + this.Address + ", is personally known to me.";
+            }
+            string preview2 = Xhe + " comes of a respectable family. " + Xhe + " " + wis + " reading in Class " + this.SelectedClass + ", Sec. " + this.SelectedSection + ", Roll " + this.Roll + ", Session " + this.Session + "-"+this.SessionEnd+".";
+            string preview3 = Hir + " date of birth as per School records is " + this.Dob + ".";
+            string preview4 = "So far I know, " + xhe + " bears a good moral character. I wish " + hir + " every success in life.";
+            this.PreviewText = preview1 + "\n  " + preview2 + "\n  " + preview3 + "\n  "+preview4;
+        }
+
+        private void Reset()
+        {
+            this.StdName = this.FatherName = this.Vill = this.PO = this.Ps = this.Dist = this.Pin = this.Address = this.Roll = this.PreviewText = string.Empty;
+            this.GenderIndex = this.SelectedClassIndex = this.SelectedSectionIndex = -1;
+        }
+
+        #endregion   
     }
 }
